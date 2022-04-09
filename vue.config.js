8e824b9620c176a5dfcf7d1b4d9ab22b009435d7
Naveenrf <naveen@rightfunds.com>
2022-04-09 11:51:34 +0530

    port changes

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: process.env.PORT || 8080
+  }
 })
