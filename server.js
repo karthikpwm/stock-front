fa9d6c5e081930c9f8cc5fe58980e96dd85b4755
Naveenrf <naveen@rightfunds.com>
2022-04-09 13:42:16 +0530

    new server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express()
 //we are configuring dist to serve site files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
- res.sendFile(path.join(__dirname, '/index.html'))
-})
+// app.get(/.*/, function (req, res) {
+//  res.sendFile(path.join(__dirname, '/index.html'))
+// })
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`site is listening on port: ${port}`)
