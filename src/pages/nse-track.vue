77ea05578dc0ca3891d9d979a99bf073601e6c58
Naveenrf <naveen@rightfunds.com>
2022-07-28 16:41:39 +0530

    bfhr

--- src/pages/nse-track.vue
+++ src/pages/nse-track.vue
@@ -253,7 +253,7 @@ import { URL } from '../helper/consts.js'
         .get(`https://yfapi.net/v6/finance/quote?region=IN&lang=en&symbols=%2C%5ECRSLDX,%5ENSEI,%2CNIFTY_MIDCAP_100.NS,%2C%5ECNXSC`, {
         headers: {
            'accept': 'application/json',
-        'X-API-KEY': 'J6WzGEj49F59U8eeu3kSr210VOXDYVCiacvOx7fS'
+        'X-API-KEY': '1yphCd0yJh9nooEWtmiFK4nMVnxx8sbE1t76fZFJ'
                 }
         }).then( (res) => {
         //console.log('res',res.data.quoteResponse.result)
